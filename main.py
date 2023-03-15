ba1ac3447f2b5f278ff2f0c2472ca25be54960fe
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-03-15 19:59:37 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 from src.logger import logging
 from src.exception import CustomException
+from src.components import data_ingestion 
 import sys
 
 
 def main():
-    try:
-        logging.info("we have entered in the main function")
-        a=6/0
-    except Exception as e:
-        raise CustomException(e, sys)
+  
+    logging.info("we have entered in the main function")
+    DataIngestion=data_ingestion.DataIngestion()
+    DataIngestion.initiate_data_ingestion()
 
 
 
 
+       
 
 
 
 
 
 if __name__=="__main__":
-
-    main()
     logging.info("logging is started")
+    main()
+    
