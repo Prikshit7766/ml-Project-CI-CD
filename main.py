5e16f554c792b5cbf03a3fc502570df262398392
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-03-19 14:03:55 +0530

    data transformation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.logger import logging
 from src.exception import CustomException
-from src.components import data_ingestion 
+from src.components import data_ingestion ,data_transformation
 import sys
 
 
@@ -8,9 +8,9 @@ def main():
   
     logging.info("we have entered in the main function")
     DataIngestion=data_ingestion.DataIngestion()
-    DataIngestion.initiate_data_ingestion()
-
-
+    Train_Path,Test_Path=DataIngestion.initiate_data_ingestion()
+    DataTransformation=data_transformation.DataTransformation()
+    train_arr,test_arr,_=DataTransformation.initiate_data_transformation(Train_Path,Test_Path)
 
 
        
